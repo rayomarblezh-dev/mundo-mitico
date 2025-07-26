847226aeb3669a164d57d341629fa0dd4605fb67
rayomarblez <rayomarblezh@protonmail.com>
2025-07-26 04:50:54 -0900

    go

--- admin_panel/wsgi.py
+++
@@ -1,7 +0,0 @@
-from app import app
-import os
-
-if __name__ == "__main__":
-    # En Railway, usar la variable PORT que asigna automáticamente
-    port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port) 
